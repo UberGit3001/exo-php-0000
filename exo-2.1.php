f57e47d4906a0bd17643ea329b1353c57aa42893
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-07-11 16:15:09 +0200

    commit de exo2.2 de php du drive

---
+++ exo-2.1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo2-php2.1</title>
+</head>
+<body>
+   <?php
+$a = 5;
+$b = 10;
+
+$som = $a + $b;
+$diff = $a - $b;
+$prod = $a * $b;
+$quot = $a / $b;
+$rest = $a % $b;
+
+echo "La somme de a et b est $som.<br>";
+echo "La différence de a et b est $diff.<br>";
+echo "Le produit de a et b est $prod.<br>";
+echo "Le quotient de a et b est $quot.<br>";
+echo "Le reste de a et b est $rest.<br>";
+?>
+
+</body>
+</html>
---
+++ exo-2.2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo2.2-php</title>
+</head>
+<body>
+    <?php
+$a = 5;
+$b = 10;
+
+echo "a == b donne " . (($a == $b) ? 'true' : 'false') . "<br>";
+echo "a != b donne " . (($a != $b) ? 'true' : 'false') . "<br>";
+echo "a > b donne " . (($a > $b) ? 'true' : 'false') . "<br>";
+echo "a < b donne " . (($a < $b) ? 'true' : 'false') . "<br>";
+echo "a >= b donne " . (($a >= $b) ? 'true' : 'false') . "<br>";
+echo "a <= b donne " . (($a <= $b) ? 'true' : 'false') . "<br>";
+?>
+
+</body>
+</html>
