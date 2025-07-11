3795697e85d438f9f884043c95fad0198556f37e
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-07-11 11:38:45 +0200

    premier commit exo-0000 php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>exo php N°0000</title>
+</head>
+<body>
+    <div class="bonjours">
+
+        <h1><?php echo"Hello World Aurélien"; ?></h1>
+        <h2><?php echo"Hello World Aurélien"; ?></h2>
+        <h3><?php echo"Hello World Aurélien"; ?></h3>
+        <h4><?php echo"Hello World Aurélien"; ?></h4>
+        <h5><?php echo"Hello World Aurélien"; ?></h5>
+        <h6><?php echo"Hello World Aurélien"; ?></h6>
+
+    </div>
+</body>
+</html>
