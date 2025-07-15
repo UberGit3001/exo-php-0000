4b45eb81bc0043f444916e39292d4e1bcc3338cc
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-07-15 10:29:04 +0200

    creation du dossier exercice N°4 pour les fichiers exos php

---
+++ Exercice-N4/exo-4.2.php
@@ -0,0 +1,13 @@
+<?php 
+    include __DIR__ . '/../inc/header.php';
+?>
+
+<h2>exo-4.2-PHP</h2>
+<section class="container">
+<?php
+ 
+  ?>
+</section>
+<?php 
+    include __DIR__ . '/../inc/footer.php';
+?>
